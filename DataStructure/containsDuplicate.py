0864770cd9f62ef8e112ea9a53ef479c6e44e167
amirsho0017 <amirshorakhimov0017>
2023-03-15 08:25:12 +0500

    fixed💋:

--- DataStructure/containsDuplicate.py
+++ DataStructure/containsDuplicate.py
@@ -6,5 +6,17 @@ class Solution(object):
         """
         return len(set(nums)) != len(nums)
 
-nums = '[' + input('Input nums: ') + ']'
+
+# creating an empty list
+nums = []
+
+# number of elements as input
+n = int(input("Enter number of elements : "))
+
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+
+    nums.append(ele)  # adding the element
+
 print(Solution.containsDuplicate(nums))
