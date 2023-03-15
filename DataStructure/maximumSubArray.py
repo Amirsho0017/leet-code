01a363f72d4e2e1871e6418c5aa0f5ed7923a1e3
amirsho0017 <amirshorakhimov0017>
2023-03-15 09:53:49 +0500

    Runtime: 625 ms, faster than 21.73% of Python online submissions for Maximum Subarray. Memory Usage: 26.2 MB, less than 96.68% of Python online submissions for Maximum Subarray.

---
+++ DataStructure/maximumSubArray.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maxSubArray(nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        len_n = len(nums)
+
+        if len_n == 1:
+            return nums[0]
+
+        sums = []
+        sums.append(nums[0])
+        max_sum = nums[0]
+
+        for i in range(1, len_n):
+            sums.append(max(sums[i - 1] + nums[i], nums[i]))
+            max_sum = max(max_sum, sums[i])
+
+        return max_sum
+
+# creating an empty list
+nums = []
+
+# number of elements as input
+n = int(input("Enter number of elements : "))
+
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+
+    nums.append(ele)  # adding the element
+
+print(Solution.maxSubArray(nums))
