a5a49e80e81ceba32ce04ba5416ab9a10fa8a212
amirsho0017 <amirshorakhimov0017>
2023-06-01 15:17:25 +0500

    refactored

--- DataStructure/medium/maximumSubArray.py
+++ DataStructure/medium/maximumSubArray.py
@@ -9,7 +9,7 @@ class Solution(object):
         if len_n == 1:
             return nums[0]
 
-        sums.append(nums[0])
+        sums = [nums[0]]
         max_sum = nums[0]
 
         for i in range(1, len_n):
